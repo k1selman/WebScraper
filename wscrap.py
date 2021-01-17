8fdace369d62e47a8ba5aa1b733aa46f5e41cd0b
k1selman <barkq113@gmail.com>
2021-01-17 20:38:28 +0100

    sourcecode

---
+++ wscrap.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 17 18:16:52 2021
+
+@author: k1selman
+
+------------------------------------------------------------------------------------
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+
+ht = urlopen('https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna')
+
+bs = BeautifulSoup(ht.read(), 'html.parser')
+
+print(bs.h3)
+
+
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+
+try:
+    ht = urlopen('https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna')
+except HTTPError as e:
+    print(e)
+except URLError as e:
+    print("Server could not be found...")
+else:
+    print("Working fine, proceeding...")
+"""
+
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    
+    try:
+        data = BeautifulSoup(html.read(), 'html.parser')
+        title = data.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+
+title = getTitle('https://en.wikipedia.org/wiki/Main_Page')
+if title == None:
+    print('Could not find title...')
+else:
+    print(title)
+
